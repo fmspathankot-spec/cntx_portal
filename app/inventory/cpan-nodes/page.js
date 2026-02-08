0c86c209f11732d3115683ebaaf7fe4832df6b36
fmspathankot-spec <fmspathankot@gmail.com>
2026-02-08 10:50:29 +0530

    Add CPAN Nodes inventory page

---
+++ app/inventory/cpan-nodes/page.js
@@ -0,0 +1,10 @@
+import CPANNodesDashboard from './CPANNodesDashboard';
+
+export const metadata = {
+  title: 'CPAN Nodes - Network Inventory',
+  description: 'CPAN network nodes inventory management system',
+};
+
+export default function CPANNodesPage() {
+  return <CPANNodesDashboard />;
+}
